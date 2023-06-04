45af4313455bd0ed8679cca1924c2c92abfaed47
al21011 <al21011@shibaura-it.ac.jp>
2023-06-05 03:35:00 +0900

    手牌の試作しました

--- Game/game.js
+++ Game/game.js
@@ -1,13 +1,33 @@
 var object = document.querySelector('.object');
-var originalTop = object.offsetTop;
+var objects = document.querySelectorAll('.object');
+var originalTop = objects[0].offsetTop - 8;
 var isMoved = false;
 
 object.addEventListener('click', function(){
+    selectTile(this);
+});
+
+// objectCountだけ牌を生成して並べる．
+var container = document.getElementById('container');
+var objectCount = 12;
+
+for(var i = 0; i < objectCount; i++){
+    var object = document.createElement('div');
+    object.className = 'object';
+    container.appendChild(object);
+
+    object.addEventListener('click', function(){
+        selectTile(this);
+    });
+}
+
+function selectTile(object){
+    // 牌が選択済みの場合isMoved==true
     if(isMoved){
-        object.style.top = originalTop + 'px';
+        object.style.top = (originalTop) + 'px';
         isMoved = false;
     } else{
         object.style.top = (originalTop - 50) + 'px';
         isMoved = true;
     }
-});
+}
