e588fbd0a1a60d5f6381b69043cdf33e0d6e1da5
jounenzi <al21102@shibaura-it.ac.jp>
2023-06-15 22:18:18 +0900

    m15のインデントの修正

--- Game/m15.js
+++ Game/m15.js
@@ -13,7 +13,9 @@ Return          : true ロン上がりする, false ロン上がりしない
 */
 function checkWinonRon(trash, trashPoint, canWinTile) {
     if (Math.floor((trash[trashPoint] % 1000) / 10) === canWinTile) {
-        if (confirm('ロン')) return true; // ロンならtrueを格納.
-        else return false;
+        if (confirm('ロン')) 
+            return true; // ロンならtrueを格納.
+        else   
+            return false;
     }
 }
