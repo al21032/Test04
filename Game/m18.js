0f5ef5e6d6cf0ca57ba4da7b49cc797bc9b3ce94
aoko14 <134358474+aoko14@users.noreply.github.com>
2023-06-27 16:24:51 +0900

    修正しました.

--- Game/m18.js
+++ Game/m18.js
@@ -1,17 +1,17 @@
 /*
-Designer:高橋匠
-Date    :2023.6.10
-Purpose :まんじゃらシステム
+Designer    :森本直寛
+Date        :2023.6.20
+Purpose     :まんじゃらシステム
 */
 
 /*
-Function Name:AfterReachDiscard
-Designer     :高橋匠
-Date         :2023.6.10
-Function     :ユーザの切り番で，ツモ牌のみを打牌する．
+Function Name   :afterReachDiscard
+Designer        :森本直寛
+Date            :2023.6.20
+Function        :打牌した後の操作を行う
 */
 
-function afterReachDiscard(trash0, trashPoint, hand) {
+function afterReachDiscard(trash0, trashPoint0, hand) {
     hand[11] -= 9000;
     trash0[trashPoint0] = hand[11];
     trashPoint0 += 1;
