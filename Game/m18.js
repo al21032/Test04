80ac2a766b7785047e76b97cbd1582364bed2dd3
jounenzi <al21102@shibaura-it.ac.jp>
2023-07-09 01:47:12 +0900

    m18の修正

--- Game/m18.js
+++ Game/m18.js
@@ -11,7 +11,7 @@ Date            :2023.6.20
 Function        :打牌した後の操作を行う
 */
 
-function afterReachDiscard(trash0, trashPoint0, hand) {
+function afterReachDiscard(trash0, trashPoint, hand) {
     hand[11] -= 9000;
     trash0[trashPoint0] = hand[11];
     trashPoint0 += 1;
